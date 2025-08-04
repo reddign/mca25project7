c3f635c3e54c0f7913c1e6089eb6f6f18b8c35cb
Omlett1234 <merrell@etown.edu>
2025-08-04 11:25:50 -0400

    makign snake

--- snake/snake.js
+++ snake/snake.js
@@ -1,20 +1,31 @@
 let canvas = document.querySelector("canvas");
 const graphics = canvas.getContext("2d");
-let fps = 500
 let sX = 0
 let sY = 0
 let snake = []
-let snakeSpeedX = blocks
 let snakeSpeedY = 0
 let round = 0
-let blocks = 20
 let foodX = 0
 let foodY = 0
 
 
+function drawblocks(){
+    for(i=0;i<round+2;i++){
+        for(a=0;a<round+2;a++){
+            graphics.fillstyle="green"
+            let blockheight = canvas.height/round+2
+            let blockwidth = canvas.width/round+2 
+            let x = i*blockwidth
+            let y = a*blockheight
+            graphics.fillRect(x,y,blockwidth,blockheight)
+        }
+    }
+}
+function drawfood(){
 
+}
 function animate(){
-
+    drawblocks();
 }
 
 function drawPart(x, y){
@@ -26,4 +37,4 @@ function drawPart(x, y){
 }
 
 
-let loop = window.setInterval(animate, fps/1000)
+let loop = window.setInterval(animate, 200)
