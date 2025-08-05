b06a9a58e28b7ecf54f54928cbb815b8a84cd626
Matttastic <LEHMAN@etown.edu>
2025-08-05 10:01:31 -0400

    huge graphics-related changes (tetris)

--- tetris/tetris.js
+++ tetris/tetris.js
@@ -4,12 +4,23 @@ let pieceQueue=[]
 let board=[]
 let timer=0
 let pieceX=0
-let pieceY=20
+let pieceY=0
 let grav=120
 let pieceShape=[[],[],[],[]]
 let pieceCurrent=""
 let queueInd=0
 let process=window.setInterval(tick,1/60)
+let pieceShapeArray=[
+    [[0,0],[-1,-1],[0,-1],[1,0]],
+    [[-1,0],[0,0],[1,0],[1,-1]],
+    [[.5,.5],[-.5,.5],[.5,-.5],[-.5,-.5]],
+    [[0,0],[0,-1],[1,-1],[-1,0]],
+    [[-1.5,-.5],[-.5,-.5],[.5,-.5],[1.5,-.5]],
+    [[-1,-1],[-1,0],[0,0],[1,0]],
+    [[-1,0],[0,0],[0,-1],[1,0]]]
+let colorArray=["red","orange","yellow","green","cyan","blue","purple"]
+setup()
+
 function tick(){
     update()
     draw()
@@ -17,11 +28,13 @@ function tick(){
 function update(){
     timer++
     if(timer%grav==0){
-        pieceY-=1
+        pieceY+=1
     }
     pieceX+=movePiece()
 }
 function draw(){
+    graphics.fillStyle="black"
+    graphics.fillRect(0,0,canvas.width,canvas.height)
     graphics.strokeStyle="dimGray"
     for(let r=0;r<20;r++){
         for(let c=0;c<10;c++){
@@ -32,16 +45,22 @@ function draw(){
     graphics.strokeRect(490,60,300,600)
     for(let r=0;r<board.length;r++){
         for(let c=0;c<10;c++){
-            if(board[c][r]!='n'){ // TODO: handle colors per mino
-                graphics.fillStyle="white"
+            if(board[c][r]!=7){ // color handling works hopefully but untested
+                graphics.fillStyle=colorArray[board[c][r]]
                 graphics.fillRect() // TODO: draw rectangles (o yeah y axis is inverted in the table btw so thats fun (its like this intentionally dont worry about it))
             }
         }
     }
     for(let i=0;i<4;i++){
-        graphics.fillStyle="white"
+        graphics.fillStyle=colorArray[pieceCurrent]
         graphics.fillRect(490+30*(pieceX+pieceShape[i][0]),60+30*(pieceY+pieceShape[i][1]),30,30)
-        console.log(490+30*(pieceX+pieceShape[i][0]),60+30*(pieceY+pieceShape[i][1]),pieceCurrent)
+    }
+    for(let i=0;i<5;i++){
+        graphics.fillStyle=colorArray[pieceQueue[queueInd+i]]
+        for(let j=0;j<4;j++){
+            console.log(pieceQueue[queueInd+j])
+            graphics.fillRect(810+10*(pieceShapeArray[pieceQueue[queueInd+i]][j][0]),80+10*(i*5+pieceShapeArray[pieceQueue[queueInd+i]][j][1]),10,10)
+        }
     }
 }
 function setup(){ // TODO: clean up & consolidate related code
@@ -49,7 +68,8 @@ function setup(){ // TODO: clean up & consolidate related code
     newPiece()
 }
 function fillQueue(){ // should work
-    let bag=['z','l','o','s','i','j','t']
+    let bag=[0,1,2,3,4,5,6]
+    //       z,l,o,s,i,j,t
     for(let i=0;i<7;i++){
         let ind=Math.floor(Math.random()*(7-i)+i)
         let temp=bag[i]
@@ -66,28 +86,17 @@ function movePiece(){ // TODO: add keyboard input + movement logic
 function newPiece(){ // should work
     queueInd++
     pieceCurrent=pieceQueue[queueInd]
-    console.log('yeah')
-    switch(pieceCurrent){
-        case 'z':
-            pieceShape=[[0,0],[-1,-1],[0,-1],[1,0]]
-        break
-        case 'l':
-            pieceShape=[[-1,0],[0,0],[1,0],[1,-1]]
-        break
-        case 'o':
-            pieceShape=[[.5,.5],[-.5,.5],[.5,-.5],[-.5,-.5]]
-        break
-        case 's':
-            pieceShape=[[0,0],[0,-1],[1,-1],[-1,0]]
-        break
-        case 'i':
-            pieceShape=[[-.5,-1.5],[-.5,-.5],[-.5,.5],[-.5,1.5]]
-        break
-        case 'j':
-            pieceShape=[[-1,-1],[-1,0],[0,0],[1,0]]
-        break
-        default:
-            pieceShape=[[-1,0],[0,0],[0,-1],[1,0]]
+    pieceShape=pieceShapeArray[pieceCurrent]
+    if(pieceCurrent==2||pieceCurrent==4){
+        pieceX=4.5
+        if(pieceCurrent==2){
+            pieceY=-1.5
+        }else{
+            pieceY=-.5
+        }
+    }else{
+        pieceX=4
+        pieceY=-1
     }
     if(queueInd+6>pieceQueue.length){
         fillQueue()
