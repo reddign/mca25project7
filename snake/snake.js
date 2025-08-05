162ed2f1ee000bc9915d20474d2a6643a845302c
Omlett1234 <merrell@etown.edu>
2025-08-05 13:19:30 -0400

    idk jus snake stuff

--- snake/snake.js
+++ snake/snake.js
@@ -5,6 +5,10 @@ apple.src = 'apple.png'
 /*  VARIABLES    */
 let round = 3;
 let blocksdrawn = 0;
+let x = 0
+let y = 0
+let foodx = 0
+let foody = 0
 
 /*  FUNCTIONS   */
 function drawblocks(){
@@ -13,19 +17,24 @@ function drawblocks(){
         for(a=0;a<round+2;a++){
             graphics.fillStyle="green"
             graphics.strokeStyle="black"
-            let blockheight = canvas.height/(round+2)
+                let blockheight = canvas.height/(round+2)
             let blockwidth = canvas.width/(round+2) 
-            let x = i*blockwidth
-            let y = a*blockheight
+            x=i*blockwidth
+            y=a*blockheight
             graphics.fillRect(x,y,blockwidth,blockheight)
             graphics.strokeRect(x,y,blockwidth,blockheight)
             blocksdrawn+=1
         }
     }
 }
+function decideBlock(change){
+    if(change==true){
+        foodx = Math.floor(Math.random()*blocksdrawn*100)
+        foody = Math.floor(Math.random()*blocksdrawn*100)
+    }
+}
 function drawFood(){
-    graphics.drawImage(apple,100,100,1250/blocksdrawn,1250/blocksdrawn)
-    
+    graphics.drawImage(apple,foodxy,foodxy,1250/blocksdrawn,1250/blocksdrawn)
 }
 function animate(){
     drawblocks();
