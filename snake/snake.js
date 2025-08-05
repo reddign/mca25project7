3953b4ea3beff12949980eb93efdd40f7d0d1879
Omlett1234 <merrell@etown.edu>
2025-08-05 09:09:16 -0400

    finished the snake blocks

--- snake/snake.js
+++ snake/snake.js
@@ -4,20 +4,21 @@ let sX = 0
 let sY = 0
 let snake = []
 let snakeSpeedY = 0
-let round = 0
+let round = 3
 let foodX = 0
 let foodY = 0
-
 
 function drawblocks(){
     for(i=0;i<round+2;i++){
         for(a=0;a<round+2;a++){
-            graphics.fillstyle="green"
-            let blockheight = canvas.height/round+2
-            let blockwidth = canvas.width/round+2 
+            graphics.fillStyle="green"
+            graphics.strokeStyle="black"
+            let blockheight = canvas.height/(round+2)
+            let blockwidth = canvas.width/(round+2) 
             let x = i*blockwidth
             let y = a*blockheight
             graphics.fillRect(x,y,blockwidth,blockheight)
+            graphics.strokeRect(x,y,blockwidth,blockheight)
         }
     }
 }
@@ -26,14 +27,6 @@ function drawfood(){
 }
 function animate(){
     drawblocks();
-}
-
-function drawPart(x, y){
-    graphics.fillStyle = brown
-    graphics.beginPath()
-    graphics.arc(x, y, radius,0, Math.PI*2)
-    graphics.fill()
-    graphics.closePath()
 }
 
 
