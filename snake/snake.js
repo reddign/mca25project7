1ce72cdedd3cc9b4f1e991138fec329b4cbfbdc0
Omlett1234 <merrell@etown.edu>
2025-08-05 10:42:00 -0400

    Added images, edited snake

--- snake/snake.js
+++ snake/snake.js
@@ -1,11 +1,14 @@
 let canvas = document.querySelector("canvas");
 const graphics = canvas.getContext("2d");
-
+const img = new Image()
+img.src = 'apple.png'
 /*  VARIABLES    */
-let round = 1
+let round = 3;
+let blocksdrawn = 0;
 
 /*  FUNCTIONS   */
 function drawblocks(){
+    blocksdrawn = 0;
     for(i=0;i<round+2;i++){
         for(a=0;a<round+2;a++){
             graphics.fillStyle="green"
@@ -16,11 +19,17 @@ function drawblocks(){
             let y = a*blockheight
             graphics.fillRect(x,y,blockwidth,blockheight)
             graphics.strokeRect(x,y,blockwidth,blockheight)
+            blocksdrawn+=1
         }
     }
 }
+function drawFood(){
+    graphics.drawImage(img,100,100,1250/blocksdrawn,1250/blocksdrawn)
+    
+}
 function animate(){
     drawblocks();
+    drawFood();
 }
 
 //used to animate, continually draws whats in animate
