1427f3a77184c26929448eff0f844b713dfc2a71
Evan Harner <HARNER@etown.edu>
2025-08-05 09:52:54 -0400

    changed function name bcuz js is dumb

--- Clicker/clicker.js
+++ Clicker/clicker.js
@@ -5,24 +5,30 @@ let points = 0
 let x = 650
 let y = 275
 let radius = 30
-drawThing()
+
 function animate(){
-    drawScore()
+    clear()
+    drawScore(points)
+    button()
 }
-function click(event){
+function one(event){
     points+=1
 }
-function drawThing(){
+function clear(){
+    graphics.fillStyle='lightBlue'
+    graphics.fillRect(0, 0, 1250, 550)
+}
+function button(){
     graphics.fillStyle ="black"
     graphics.beginPath()
     graphics.arc(x, y, radius,0, Math.PI*2)
     graphics.fill()
     graphics.closePath()
 }
-function drawScore(){
+function drawScore(points){
     graphics.fillStyle ='black'
     graphics.font = "bold 24px 'Arial', serif"
     graphics.fillText("Points: "+ points, 10, 30,100)
 }
 
-let loop = window.setInterval(animate, .5)
+let loop = window.setInterval(animate, 16)
